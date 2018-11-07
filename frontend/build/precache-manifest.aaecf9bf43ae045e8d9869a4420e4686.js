e88326b862ff1e313c789875b133e202d410bb9a
Neal Shen <>
2018-11-07 17:44:29 +1100

    first commit

---
+++ frontend/build/precache-manifest.aaecf9bf43ae045e8d9869a4420e4686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9816ff66d7fc99f3c361",
+    "url": "/static/css/main.fc87d91d.chunk.css"
+  },
+  {
+    "revision": "9816ff66d7fc99f3c361",
+    "url": "/static/js/main.9816ff66.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06c7cb3bb58e5f6c045945d1b88d009e",
+    "url": "/index.html"
+  }
+];
